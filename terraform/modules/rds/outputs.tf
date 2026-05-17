5ce17c2403ddc34eb0327d2969bb5edd6021b60e
IshKevin <carterk279@gmail.com>
2026-05-17 16:54:22 +0200

    feat: add outputs for RDS instance including address, port, and database name

---
+++ terraform/modules/rds/outputs.tf
@@ -0,0 +1,14 @@
+output "address" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.this.address
+}
+
+output "port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.this.port
+}
+
+output "db_name" {
+  description = "Database name"
+  value       = aws_db_instance.this.db_name
+}
